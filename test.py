9c2c1da757155f97141cd380e2daba2a74bbef2b
William <lichengqi0805@gmail.com>
2020-09-21 19:45:08 +0800

    update disparity map

--- test.py
+++ test.py
@@ -4,7 +4,9 @@ import glob
 import os
 import matplotlib.pyplot as plt
 import time
-filepath = '/home/william/Documents/Stereo_Camera_Measurement/Dataset/'
+import scipy.signal as signal
+
+filepath = os.path.join(os.getcwd(),'Dataset/')
 
 # for file_name in (glob.glob(filepath + 'right/*.jpeg')):
 #     file_num = file_name.split('/')[-1].split(' ')[-1].split('.')[0]
@@ -18,8 +20,8 @@ filepath = '/home/william/Documents/Stereo_Camera_Measurement/Dataset/'
 #     disparity = stereo.compute(imgL,imgR)
 #     plt.imshow(disparity,'gray')
 #     plt.show()
-imgR = cv2.imread("/home/william/Documents/Stereo_Camera_Measurement/Dataset/right.png", 0)
-imgL = cv2.imread("/home/william/Documents/Stereo_Camera_Measurement/Dataset/left.png", 0)
+imgR = cv2.imread(filepath + "right.png", 0)
+imgL = cv2.imread(filepath + "left.png", 0)
 imgL = imgL[0:482,0:360] 
 imgR = imgR[0:482,0:360]  
 win_size = 5
@@ -39,9 +41,14 @@ stereo = cv2.StereoSGBM_create(minDisparity= min_disp,
 #Compute disparity map
 print ("\nComputing the disparity  map...")
 disparity_map = stereo.compute(imgR, imgL)
-
+disparity_map = signal.medfilt(disparity_map,(3,3)) # Median Filtering
 #Show disparity map before generating 3D cloud to verify that point cloud will be usable. 
-plt.imshow(disparity_map,'gray')
+plt.figure()
+plt.imshow(disparity_map,'summer')
+kernel = np.ones((2,2),np.uint8)  
+erosion = cv2.erode(disparity_map,kernel,iterations = 1)
+plt.figure()
+plt.imshow(erosion,'summer')
 plt.show()
 plt.close('all')
 
