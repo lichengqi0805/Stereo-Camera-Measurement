782ff10b294419d13b0da470c77eee798ca039d7
William <lichengqi0805@gmail.com>
2020-09-22 09:23:11 +0800

    merge

--- Dataset/Capture_Stereo_Data.py
+++ Dataset/Capture_Stereo_Data.py
@@ -2,8 +2,8 @@ import cv2
 import numpy as np
 import os
 
-cap_1 = cv2.VideoCapture(1)
-cap_2 = cv2.VideoCapture(0)
+cap_1 = cv2.VideoCapture(0)
+cap_2 = cv2.VideoCapture(1)
 index = 1
 while True:
     ret_1, frame_1 = cap_1.read()
@@ -12,9 +12,9 @@ while True:
     cv2.imshow("CAM 2", frame_2)
     input = cv2.waitKey(1) & 0xFF
     if input == ord('x'):
-        cv2.imwrite("124/cam_1 %d.jpeg" % (index), frame_1)
+        cv2.imwrite("/Users/William/work_space/Stereo-Camera-Measurement/Dataset/left/%d.jpeg" % (index), frame_1)
         print("Picture taken %d" % (index))
-        cv2.imwrite("124/cam_2 %d.jpeg" % (index), frame_2)
+        cv2.imwrite("/Users/William/work_space/Stereo-Camera-Measurement/Dataset/right/%d.jpeg" % (index), frame_2)
         index += 1
     if input == ord('q'):
         break
